5769b86ce77e1d35ef8301bbd32fc62088002900
wyq <13337839387@163.com>
2022-04-12 20:48:13 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git test3");
         System.out.println("git test4");
         System.out.println("master test4");
+        System.out.println("push test");
     }
 }
